b4c5d1562920e28cfb913941cf234b884f2927c6
KBierman <100715811+KBierman@users.noreply.github.com>
2024-03-05 09:50:28 -0700

    this is a stinky

---
+++ Enchantorium_v1/message_queue/get_views/app.py
@@ -0,0 +1,28 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+from os import getenv
+from uuid import uuid4
+import json
+
+#WORKING
+region_name = getenv('APP_REGION')
+enchantorium_views = boto3.resource('dynamodb', region_name=region_name ).Table('Enchantorium_Views')
+
+def lambda_handler(event, context):
+    if ( ("pathParameters" in event) ):
+        path = event["pathParameters"]
+        if path is not None and "ID" in path:
+            id = path["ID"]
+            output = enchantorium_views.get_item(Key={"productID":id})['Item']['count']
+            return response(200, output)
+    return response(401, "Missing pathID.")
+
+
+def response(code, body):
+    return {
+        "statusCode": code,
+        "headers": {
+            "Content-Type": "application/json"
+            },
+        "body": json.dumps(body),
+    }
---
+++ Enchantorium_v1/message_queue/view_consumer/app.py
@@ -0,0 +1,27 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+from os import getenv
+from uuid import uuid4
+import json
+
+sqs = boto3.resource('sqs')
+region_name = getenv('APP_REGION')
+enchantorium_creatures = boto3.resource('dynamodb', region_name=region_name ).Table('Enchantorium_Views')
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        body = json.loads(record['body'])
+        newID = str(uuid4())
+        userID = event["userID"]
+        productID = event["productID"]
+        enchantorium_creatures.put_item(Item ={
+            "ID": newID,
+            "userID": userID,
+            "productID": productID
+        })
+        print(f"Received message: {body}")
+        # Add your processing logic here
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Messages processed successfully!')
+    }
